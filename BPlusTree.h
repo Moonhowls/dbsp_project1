39ff79a38e1f652de71f02e5d1159ddc7304602e
Ka1eidosc0pe <adi.selvam99@gmail.com>
2024-03-07 15:43:08 +0800

    Test refactor of BPlusTree header

--- BPlusTree.h
+++ BPlusTree.h
@@ -1,33 +1,39 @@
 #ifndef BPLUSTREE_H
 #define BPLUSTREE_H
 
-#include "Node.h"
 #include "disk.h"
-#include <tuple>
-#include <vector>
 
 using namespace std;
 
-typedef tuple<vector<Node*>, bool> searchResult;
-typedef unsigned int uint;
-typedef unsigned char uchar;
-typedef vector<tuple<string, uchar*, int>> keyVector;
-typedef tuple<bool, uint, void*, void*> updateParentTuple;
+struct BPlusTreeNode {
+    bool isLeafNode; // flag indicating whether/not node is a leaf node
+    int numKeys; // number of keys stored in the node
+    vector<int> keys; // keys stored in this node
+    vector<vector<Record*>> recordLists; // list of records associated with each key (for leaf nodes only) (needed for duplicate handling)
+    vector<BPlusTreeNode*> children; // Pointers to children nodes (for internal nodes only)
+    BPlusTreeNode* parent; // Pointer to parent node
+};
 
 class BPlusTree {
+    private:
+        BPlusTreeNode* root; // Pointer to root node
+        int order; // Order of the B+ tree
+        int minKeys; // Min number of keys in a node
+        int maxKeys; // Max number of keys in a node
 
     public:
-        Node* rootNode;
-        unsigned int numNodes;
+        BPlusTree(int order); // constructor
+        ~BPlusTree(); // destructor
 
-        BPlusTree();
+        Record* search(int key); // Function to search for a key in the B+ tree
+        void remove(int key); // Function to delete a key and its corresponding record from the B+ tree
 
-        // methods
-        searchResult searchTargetKey(uint targetKey);
-        Node* insertTargetKey(vector<Node*> ancestryTree, Record targetRecord, uchar* blockAddress, int offset, bool isDuplicate, int paramN);
-        updateParentTuple insertKeyPtrPairToLeafNode(Node* targetLeafNode, Node::KeyPtrPair targetKeyPtrPair, int paramN);
-        vector<Node*> searchKeyRange(uint lowerBound, uint upperBound);
-        bool deleteTargetKey(vector<Node*> ancestryTree, uint targetKey);
+        // Helper functions for insertion and deletion
+        void insertIntoNode(BPlusTreeNode* node, int key, Record* record);
+        void splitNode(BPlusTreeNode* node);
+        BPlusTreeNode* findLeafNode(int key);
+        void deleteFromNode(BPlusTreeNode* node, int key);
+        void mergeNodes(BPlusTreeNode* left, BPlusTreeNode* right);
 };
 
 #endif /* BPLUSTREE_H */
